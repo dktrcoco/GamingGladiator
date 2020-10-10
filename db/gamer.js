2c09b806df036410f99e74c9889642297d900cd5
Chris Kabana <chris.kabana@gmail.com>
2020-10-10 09:18:29 -0500

    flushing out a model

--- db/gamer.js
+++ db/gamer.js
@@ -1,5 +1,12 @@
 var db = require("./models");
 
 db.Gamer.create({
-    
-})
+    gamerName: "Tester",
+    gamerNumber: "1234",
+    medalsBronze: 1,
+    medalsSilver: 2,
+    medalsGold: 3,
+    medalsTotal: 6
+}).then(function(dbGamer) {
+    console.log(dbGamer);
+});
